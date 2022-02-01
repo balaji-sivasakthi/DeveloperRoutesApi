46b9a27d516556a2fd34aa52fb78517a412c315a
balaji <balajisivasakthi@gmail.com>
2022-02-01 22:12:56 +0530

    course model added

---
+++ models/Course.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const courseSchema = new mongoose.Schema(
+  {
+    _id:mongoose.Schema.Types.ObjectId,
+    title:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+    playList:{
+        tyoe:Array,
+        required:true
+    },
+    rate:{
+        type:Number,
+        default:0
+    }
+  },{timestamps:true}
+);
+
+const course = new mongoose.model("course", courseSchema);
+
+module.exports = course;
